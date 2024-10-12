cfe88c8eea4c8069b498f0a291c35aab41f4da88
AdarshVishwakarma <38201144+proaddy@users.noreply.github.com>
2024-10-12 11:48:27 +0530

    editor fixed

--- src/pages/Editnote.jsx
+++ src/pages/Editnote.jsx
@@ -1,8 +1,8 @@
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import { ClassicEditor, Bold, Essentials, Italic, Mention, Paragraph, Undo } from 'ckeditor5';
+import 'ckeditor5/ckeditor5.css';
 // import { SlashCommand } from 'ckeditor5-premium-features';
 
-import 'ckeditor5/ckeditor5.css';
 import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useDashboardContext } from '../components/DashboardContext';
@@ -16,21 +16,46 @@ export default function Editnote() {
     const { folderStructure, setFolderStructure } = useDashboardContext();
 
     function searchFolder(folderlist, id) {
-        
+        folderlist.forEach(element => {
+            if(element.type === 'file') {
+                if(element.id === id) {
+                    setText(element.desc)
+                }
+            }
+            else if(element.type === 'folder') {
+                searchFolder(element.list, id)
+            }
+        });
+    }
+
+    function savechanges(folderlist, id, text) {
+        // console.log(folderlist, id, text);
+        return folderlist.map(element => {
+            if(element.type === 'file' && element.id === id) {
+                console.log(element);
+                return {...element, desc: text};
+            } else if(element.type === 'folder') {
+                return {...element, list: savechanges(element.list, id, text)};
+            } else {
+                return element;
+            }
+        })
     }
 
     const [text, setText] = useState();
 
-    useEffect(()=>{
-        setText(desc);
-    }, []);
-
     const navigate = useNavigate();
 
     const handleClick = () => {
+        const newfolderStructure = savechanges(folderStructure, id, text);
+        setFolderStructure(newfolderStructure)
+        // console.log(newfolderStructure);
         navigate('/')
     }
 
+    useEffect(()=>{
+        searchFolder(folderStructure, id)
+    }, []);
 
     return (
       <div className='flex flex-col'>
